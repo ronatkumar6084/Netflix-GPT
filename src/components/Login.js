a00bdabb9a4c4c43ec62887c50cbcdefb72d0709
ronatkumar6084 <ronatkumarmuni42@gmail.com>
2024-03-26 20:41:29 +0530

    created backend as google firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from 'react'
 import Header from './Header'
 import { checkValidData } from '../utils/validate';
+import {createUserWithEmailAndPassword,signInWithEmailAndPassword} from "firebase/auth";
+import { auth } from '../utils/firebase';
 
 const Login = () => {
      const [isSigninForm, setIsSigninForm] = useState(true);
@@ -19,6 +21,35 @@ const Login = () => {
         const message = checkValidData(email.current.value, password.current.value);
         console.log(message);
         setErrorMessage(message);
+        if(message) return;
+         
+        //Signin Signup logic
+        if(!isSigninForm){
+            //Signup logic
+            createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+            .then((userCredential) => {
+            const user = userCredential.user;
+            console.log(user);
+            })
+           .catch((error) => {
+               const errorCode = error.code;
+               const errorMessage = error.message;
+               setErrorMessage(errorCode + "---" + errorMessage);
+             });
+        }
+        else{
+            //Signin logic
+            signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+           .then((userCredential) => {
+            const user = userCredential.user;
+            console.log(user);
+            })
+           .catch((error) => {
+           const errorCode = error.code;
+           const errorMessage = error.message;
+           setErrorMessage(errorCode + "---" + errorMessage);
+           });
+        }
     };
 
   return (
